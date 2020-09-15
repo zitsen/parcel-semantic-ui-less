228cafd683dd3f8367e0b54f3fd07ea3ff7efbbf
Huo Linhe <linhehuo@gmail.com>
2020-09-16 00:06:58 +0800

    chore: remove cli in examples

--- examples/cli.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import Demo from '../src/index'
-
-let demo = <Demo />
-
-let strings = ReactDOMServer.renderToString(demo)
-
-console.log(strings)
